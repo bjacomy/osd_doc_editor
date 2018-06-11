f9634422eff33c82caaec07235e5edf87552f04d
Imed Eddine Zaghdane <imededdine.zaghdane@orange.com>
2018-06-11 11:50:36 +0200

    add publish.sh file

---
+++ publish.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+AF_REPO_FOR_ES="https://sfy-metriks-registry-prod.af.multis.p.fti.net/sfy-idem_generic_estack/kibana/plugins"
+
+PLUGINS=(
+  "labeling-plugin" "labeling-plugin-6.2.2-1.zip" ""
+)
+
+if [ -z "$USER" ]
+then
+    echo "*** Error: variable USER is not set." >&2
+    exit 1
+fi
+
+echo -n LDAP Password:
+read -s password
+echo
+
+function upload() {
+  for index in $(seq 0 3 $(( ${#PLUGINS[@]} - 3 ))); do
+    src="./build/${PLUGINS[$(( $index + 1 ))]}"
+    if [[ -f "${src}" ]]; then
+      curl -kL -u "${USER}:${password}" -T "${src}" "${AF_REPO_FOR_ES}/${PLUGINS[$index]}/${PLUGINS[$(( $index + 1 ))]}"
+    else
+      echo "WARNING: Plugin '${src}' doesn't exists"
+    fi
+  done
+}
+
+upload
+

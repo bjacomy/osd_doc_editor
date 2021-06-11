cb824a786bd48658e76fb848abe203ad23db9d61
bjacomy <bjacomy.ext@orange.com>
2021-06-11 10:36:40 +0200

    plugin enable/disable with doc-editor: true/false in kibana.yml

--- public/index.ts
+++ public/index.ts
@@ -1,8 +1,9 @@
+import { PluginInitializerContext } from 'src/core/public/plugins';
 import { DocEditorPlugin } from './plugin';
 
 // This exports static code and TypeScript types,
 // as well as, Kibana Platform `plugin()` initializer.
-export function plugin() {
-  return new DocEditorPlugin();
+export function plugin(initializerContext: PluginInitializerContext) {
+  return new DocEditorPlugin(initializerContext);
 }
 export { DocEditorPluginSetup, DocEditorPluginStart } from './types';
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,22 @@
-import { PluginInitializerContext } from '../../../src/core/server';
+import { schema, TypeOf } from '@kbn/config-schema';
+import type { PluginConfigDescriptor } from 'kibana/server';
+import {  PluginInitializerContext } from '../../../src/core/server';
 import { DocEditorPlugin } from './plugin';
 
 //  This exports static code and TypeScript types,
 //  as well as, Kibana Platform `plugin()` initializer.
 
+const configSchema = schema.object({
+  enabled: schema.boolean({ defaultValue: true })
+});
+
+export const config: PluginConfigDescriptor<ConfigType> = {
+  exposeToBrowser: {
+    enabled: true
+  },
+  schema: configSchema,
+};
+export   type ConfigType = TypeOf<typeof configSchema>;
 export function plugin(initializerContext: PluginInitializerContext) {
   return new DocEditorPlugin(initializerContext);
 }
